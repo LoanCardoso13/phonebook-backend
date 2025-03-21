43f85ced5b272e4610b3e723d9c48e68ed074cbc
Loan Cardoso <jow13_122@yahoo.com>
2025-03-21 11:29:52 -0400

    Adding frontend.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
+app.use(express.static('dist'));
 
 const persons = [
     { 
